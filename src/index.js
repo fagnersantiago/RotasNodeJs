d76d99a29c795e60e056863ad8e30fc48017099c
fagnersantiago <fagner.santiago@hotmail.com>
2021-01-05 16:32:40 -0300

    testando conexão com mongoDb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const { v4: uuidv4 } = require("uuid");
-
 const app = express();
+const Users = require("./user");
+const mongoose = require("mongoose");
+
 
 app.use(express.json());
 
-const projects = [];
+const url = "mongodb+srv://admin:fagner060688@cluster0.jvjxd.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+const option = { poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true };
+
+mongoose.connect(url, option);
+
+mongoose.connection.on("error", (err) => {
+  console.log("fail connection" + err);
+});
+
+mongoose.connection.on("disconnect", () => {
+  console.log("Disconnect ");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected!");
+});
+
+module.exports = mongoose;
+// const projects = [];
 
 function handleLogRequest(request, response, next) {
   const { method, url } = request;
@@ -17,69 +37,72 @@ function handleLogRequest(request, response, next) {
   return next();
 }
 
-app.use(handleLogRequest);
+//app.use(handleLogRequest);
 
 app.get("/", (request, response) => {
-  // const {title, owner} = request.query;
+  Users.find({}, (err, data) => {
+    if (err) return response.json("user not found");
 
-  // console.log(title)
-  // console.log(owner)
-
-  // return response.json([
-  //     'project 1',
-  //     'project 2',
-  //     'project 3',
-  // ])
-
-  return response.json(projects);
+    return response.json(data);
+  });
 });
 
-app.post("/projects", (request, response) => {
-  const { title, owner } = request.body;
-  const project = { id: uuidv4(), title, owner };
+app.post("/user", handleLogRequest, (request, response) => {
+  const { email, password } = request.body;
 
-  projects.push(project);
+  if (!email || !password) return response.json("invalid data");
 
-  return response.json(project);
+  Users.findOne({ email }, (err, data) => {
+    if (err) return response.json("error");
+    if (data) return response.json("user already exist");
+
+    Users.create(request.body, (err, data) => {
+      if (err) return response.json(err);
+
+      return response.json(data);
+    });
+  });
 });
 
-app.put("/projects/:id", (request, response) => {
-  const { id } = request.params;
-  const { title, owner } = request.body;
+// app.put("/projects/:id", handleLogRequest,(request, response) => {
+//   const { id } = request.params;
+//   const { title, owner } = request.body;
 
-  const projectIndex = projects.findIndex((project) => project.id === id);
+//   const projectIndex = projects.findIndex((project) => project.id === id);
 
-  if (projectIndex === -1) {
-    return response.status(400).json({ error: "project not found." });
-  }
+//   if (projectIndex === -1) {
+//     return response.status(400).json({ error: "project not found." });
+//   }
 
-  const project = {
-    id,
-    title,
-    owner,
-  };
+//   const project = {
+//     id,
+//     title,
+//     owner,
+//   };
 
-  projects[projectIndex] = project;
+//   projects[projectIndex] = project;
 
-  return response.json(project);
-});
+//   return response.json(project);
+// });
 
-app.delete("/projects/:id", (request, response) => {
-  const { id } = request.params;
+// app.delete("/projects/:id", (request, response) => {
+//   const { id } = request.params;
 
-  const projectId = projects.findIndex((project) => project.id === id);
+//   const projectId = projects.findIndex((project) => project.id === id);
 
-  if (projectId !== id) {
-    return response
-      .status(400)
-      .json({ error: "Project not found or Id does not exist" });
-  }
+//   if (projectId !== id) {
+//     return response
+//       .status(400)
+//       .json({ error: "Project not found or Id does not exist" });
+//   }
 
-  projects.splice(projectId, 1);
+//   projects.splice(projectId, 1);
 
-  return response.status(204).send();
-});
+//   return response.status(204).send();
+// });
 
 app.listen(3333, () => {
   console.log("🚀 Back-end started on port 3333!");
 });
+
+module.exports = app;
